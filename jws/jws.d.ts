8e5d36a186a9bc2cd6e4bed39ac6d604baabd937
Tim Papler <tim.papler@3fs.si>
2014-09-08 08:31:38 +0200

    Added optional settings and made only ext module

--- jws/jws.d.ts
+++ jws/jws.d.ts
@@ -1,21 +1,21 @@
 /// <reference path="../node/node.d.ts" />
 
-declare module jws {
-    import Stream = require('stream');
-    export class VerifyStream extends Stream {
+declare module "jws" {
+    import stream = require("stream");
+    export interface VerifyStream extends stream.Stream {
         verify():boolean;
     }
-    export class SignStream extends Stream {
+    export interface SignStream extends stream.Stream {
         sign():string;
     }
 
     export var ALGORITHMS:string[];
     export interface Options {
         header : {alg:string};
-        payload: any;
-        key: any;
-        secret: string;
-        privateKey: any;
+        payload?: any;
+        key?: any;
+        secret?: string;
+        privateKey?: any;
     }
     export function createSign(options:Options):SignStream;
     export function createVerify(options:Options):VerifyStream;
@@ -24,8 +24,4 @@ declare module jws {
     export function verify();
     export function decode();
     export function isValid();
-}
-
-declare module "jws" {
-    export = jws;
 }
